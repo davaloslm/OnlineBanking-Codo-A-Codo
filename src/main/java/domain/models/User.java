251bd641b949af00205647be6d132254a05c6a2c
davaloslm <davaloslm@hotmail.com.ar>
2023-07-15 17:32:23 -0300

    Add user model

---
+++ src/main/java/domain/models/User.java
@@ -0,0 +1,17 @@
+package domain.models;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Entity
+@Data
+public class User {
+
+    private Long id;
+
+    private String username;
+
+    private String password;
+
+
+}
